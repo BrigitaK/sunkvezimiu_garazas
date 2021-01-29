7fca810ec7848652cecd4f215fdbd2c32da8dc64
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-29 22:10:16 +0200

    MechanicController create and store function

--- src/Controller/MechanicController.php
+++ src/Controller/MechanicController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Mechanic;
 
 class MechanicController extends AbstractController
 {
@@ -24,5 +26,22 @@ class MechanicController extends AbstractController
     {
         return $this->render('mechanic/create.html.twig', [
         ]);
+    }
+    /**
+     * @Route("/mechanic/store", name="mechanic_store", methods={"POST"})
+     */
+    public function store(Request $r): Response
+    {
+        $mechanic= New Mechanic;
+        $mechanic->
+        setName($r->request->get('mechanic_name'))->
+        setSurname($r->request->get('mechanic_surname'));
+
+       
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($mechanic);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('mechanic_index');
     }
 }
