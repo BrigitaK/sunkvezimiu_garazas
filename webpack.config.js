5a269adfa716c7348bb07285243381c8deb22f03
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-02-05 08:05:02 +0200

    summernote

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ Encore
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
-    .disableSingleRuntimeChunk()
+
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     //.splitEntryChunks()
@@ -32,6 +32,7 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     //.enableSingleRuntimeChunk()
+    .disableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
